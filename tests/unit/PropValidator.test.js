32814580e4c9cba0cc39ed3b4779cbab8846ba87
Steve Browning <foozlereducers@gmail.com>
2023-11-16 16:38:28 -0500

    Completed unit testing for PropValidator

--- tests/unit/PropValidator.test.js
+++ tests/unit/PropValidator.test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import { PropsValidator } from '../../validators/PropsValidator.js';
 
-test("getVal should return the constructor given value", async t=> {
+test("getVal should return the constructor given parameter value", async t=> {
     var undefinedVal;
     const PV = new PropsValidator(undefinedVal, 'StoryPoints');
     t.is(undefined, PV.getVal());
@@ -9,9 +9,10 @@ test("getVal should return the constructor given value", async t=> {
     t.is(4, PV2.getVal());
 })
 
-test("getErrors should return concatenated errors", async t=> {
+test("Errors can be concatenate between multiple propsValidators", async t=> {
     var undefinedVal;
     const PV = new PropsValidator(undefinedVal, 'StoryPoints');
+    // Notice passing on the errors to the next PropsValidator
     const PV2 = new PropsValidator('', 'StoryPoints', PV.getErrors());
     t.is('StoryPoints is undefined and is required. StoryPoints is wrong, it has zero length. ', PV2.getErrors());
 })
@@ -29,4 +30,20 @@ test("The props validator shouldn't crash and verify undefined val ", async t=> 
     t.is('StoryPoints is undefined and is required. ', errors)
 })
 
-// test("The props validator should create an error a zero length v")
+test("The props validator errorSwitch() when an error is added this reads true", async t=> {
+    var undefinedVal;
+    const PV = new PropsValidator(undefinedVal, 'StoryPoints');
+    t.true(PV.getErrorSwitch())
+})
+
+test("The WrongValue method can append additional error messaging", async t=> {
+    var undefinedVal;
+    const PV = new PropsValidator('', 'StoryPoints');
+    const error =  PV.wrongValue()
+    t.is(error, 'StoryPoints is wrong, it has zero length. ')
+    const error2 = PV.wrongValue('The value should be 0,1,2,3,5,8 or 13');
+    t.is (
+        'StoryPoints is wrong, it has zero length. The value should be 0,1,2,3,5,8 or 13',
+        error2
+    )
+})
