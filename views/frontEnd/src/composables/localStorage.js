2f5130258bc3fc49571a70b5357c6e1868adf316
Steve Browning <foozlereducers@gmail.com>
2024-04-08 16:49:32 -0400

    Added setter and getter for isMobile into local storage.

--- views/frontEnd/src/composables/localStorage.js
+++ views/frontEnd/src/composables/localStorage.js
@@ -4,12 +4,25 @@ export class LocalStorage {
     }
 
     saveAuthCredentials(userData, storageKey='aeprUser') {
+        userData.token = null;
         localStorage.setItem(storageKey,  JSON.stringify(userData))
         console.log('userData stringified', localStorage.getItem(storageKey)); 
     }
 
     getAuthData(storageKey='aeprUser') {
         return JSON.parse(localStorage.getItem(storageKey));
+    }
+
+    saveIsMobile(state, storageKey='aeprMobile') {
+        localStorage.setItem(storageKey, JSON.stringify({isMobile: state}))
+    }
+
+    getIsMobile(storageKey='aeprMobile') {
+        const isMobileObj = JSON.parse(localStorage.getItem(storageKey))
+        if (isMobileObj.isMobile) {
+            return isMobileObj.isMobile
+        }
+        return false;
     }
 
     mergeObjs(aObj, bObjs) {
