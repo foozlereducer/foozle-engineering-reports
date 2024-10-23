121eb33f2d114db01d413e3032a4018a99e335ca
Steve Browning <foozlereducers@gmail.com>
2024-10-23 15:25:37 -0400

    Removed the console log from the navigation ( as it was annoying and unnessassary. I also hooked up the axios inteceptor to redirect non-200 http requests to the login page

--- views/frontEnd/src/composables/navigation.js
+++ views/frontEnd/src/composables/navigation.js
@@ -1,8 +1,6 @@
 export function navigateTo(router, routeName) {
     if (router.currentRoute.value.name !== routeName) {
         router.push({ name: routeName });
-    } else {
-        console.log(`Already on the ${routeName} page`);
     }
 }
   
