50438bf795cf93688db9b43f3ebb71463116154f
Steve Browning <foozlereducers@gmail.com>
2024-10-23 17:17:24 -0400

    Added a spinner to the Nav while it is loading. This spinner component can be reused across the app whenever data or anything else needs to load

--- views/frontEnd/src/components/Nav.vue
+++ views/frontEnd/src/components/Nav.vue
@@ -1,106 +1,136 @@
 <template>
-    <nav>
-        <ul v-if="isAuthenticated" v-show="!authStore.isMobile" class="navigation">
-            <li><router-link class='link' :to="{ name: 'Home' }" >Home</router-link></li>
-            <li><router-link class='link' :to="{ name: 'sampleMetric' }">SampleMetric</router-link></li>
-            <li><router-link class='link' :to="{ name: '' }" >Metrics</router-link></li>
-            <li><router-link class='link' :to="{ name: '' }" >Tools</router-link></li>
-        </ul> 
-        <div class="icon">
-            <i 
-                @click="toggleMobileNav" 
-                v-show="authStore.isMobile" 
-                class="far fa-bars" 
-                :class="{'icon-active': mobileNav}">
-            </i>
-            <transition name="mobile-nav" >
-                <ul v-show="mobileNav" class="dropdown-nav">
-                    <li>
-                        <Auth @click="toggleMobileNav" class='link'/>
-                    </li>
-                    <li v-if="isAuthenticated"><router-link 
-                        @click="toggleMobileNav"
-                        class='link' :to="{ name: 'Home' 
-                    }" >Home</router-link></li>
-                    <li v-if="isAuthenticated"><router-link 
-                        @click="toggleMobileNav" 
-                        class='link' :to="{ name: 'sampleMetric' 
-                    }">SampleMetric</router-link></li>
-                    <li v-if="isAuthenticated"><router-link 
-                        @click="toggleMobileNav" 
-                        class='link' :to="{ name: '' 
-                    }" >Metrics</router-link></li>
-                    <li v-if="isAuthenticated"><router-link 
-                        @click="toggleMobileNav"
-                        class='link' :to="{ name: '' 
-                    }" >Tools</router-link></li>
-                </ul>
-            </transition>
-        </div>
+    <!-- Show the loading spinner when data is loading -->
+    <LoadingSpinner :loading="isLoading" />
+  
+    <!-- Main nav content, only visible when loading is finished -->
+    <nav v-if="!isLoading">
+      <ul v-if="isAuthenticated" v-show="!authStore.isMobile" class="navigation">
+        <li><router-link class="link" :to="{ name: 'Home' }">Home</router-link></li>
+        <li><router-link class="link" :to="{ name: 'sampleMetric' }">SampleMetric</router-link></li>
+        <li><router-link class="link" :to="{ name: '' }">Metrics</router-link></li>
+        <li><router-link class="link" :to="{ name: '' }">Tools</router-link></li>
+      </ul>
+      <div class="icon">
+        <i
+          @click="toggleMobileNav"
+          v-show="authStore.isMobile"
+          class="far fa-bars"
+          :class="{ 'icon-active': mobileNav }"
+        ></i>
+        <transition name="mobile-nav">
+          <ul v-show="mobileNav" class="dropdown-nav">
+            <li>
+              <Auth @click="toggleMobileNav" class="link" />
+            </li>
+            <li v-if="isAuthenticated">
+              <router-link
+                @click="toggleMobileNav"
+                class="link"
+                :to="{ name: 'Home' }"
+                >Home</router-link
+              >
+            </li>
+            <li v-if="isAuthenticated">
+              <router-link
+                @click="toggleMobileNav"
+                class="link"
+                :to="{ name: 'sampleMetric' }"
+                >SampleMetric</router-link
+              >
+            </li>
+            <li v-if="isAuthenticated">
+              <router-link
+                @click="toggleMobileNav"
+                class="link"
+                :to="{ name: '' }"
+                >Metrics</router-link
+              >
+            </li>
+            <li v-if="isAuthenticated">
+              <router-link
+                @click="toggleMobileNav"
+                class="link"
+                :to="{ name: '' }"
+                >Tools</router-link
+              >
+            </li>
+          </ul>
+        </transition>
+      </div>
     </nav>
-</template>
-
-<script setup>
-import { useAuthStore } from '../stores/authStore';
-import { ref, onMounted, onUnmounted, watch} from 'vue';
-import Auth from './Auth.vue';
-import {LocalStorage} from '../composables/localStorage.js';
-
-const authStore = useAuthStore();
-const windowWidth = ref(window.innerWidth);
-const windowHeight = ref(window.innerHeight);
-
-const LS = new LocalStorage(authStore)
-const userData = LS.getAuthData()
-let mobile = ref(false);
-let mobileNav = ref(false);
-let isAuthenticated = ref(false)
-
-
-onMounted(()=>{
-isAuthenticated.value = authStore.isAuthenticated;
-handler();
-});
-
-function toggleMobileNav() {
-mobileNav.value = !mobileNav.value;
-}
-
-watch(() => authStore.isAuthenticated, (newValue) => {
-isAuthenticated.value = newValue;
-}, { immediate: true });
-
-useWindowResize();
-function handler() {
+  </template>
+  
+  <script setup>
+  import { useAuthStore } from '../stores/authStore';
+  import { ref, onMounted, onUnmounted, watch, onBeforeMount } from 'vue';
+  import Auth from './Auth.vue';
+  import { LocalStorage } from '../composables/localStorage.js';
+  import LoadingSpinner from './LoadingSpinner.vue';
+  
+  // Reactive state variables
+  const isLoading = ref(true); // Initially set to true to show loading spinner
+  const authStore = useAuthStore();
+  const windowWidth = ref(window.innerWidth);
+  const windowHeight = ref(window.innerHeight);
+  
+  const LS = new LocalStorage(authStore);
+  let mobileNav = ref(false);
+  let isAuthenticated = ref(authStore.isAuthenticated);
+  
+  // Called when the component mounts
+  onMounted(() => {
+    handler(); // Handle the initial window size to determine mobile or not
+  });
+  
+  onBeforeMount(() => {
+    isLoading.value = true;
+  })
+  // Watch for changes in authentication status
+  watch(
+    () => authStore.isAuthenticated,
+    (newValue) => {
+      isAuthenticated.value = newValue;
+      if (newValue) {
+        isLoading.value = false; // Stop loading spinner once authenticated
+      }
+    },
+    { immediate: true }
+  );
+  
+  // Toggles the mobile navigation state
+  function toggleMobileNav() {
+    mobileNav.value = !mobileNav.value;
+  }
+  
+  // Window resize handler
+  useWindowResize();
+  function handler() {
     windowWidth.value = window.innerWidth;
     windowHeight.value = window.innerHeight;
-
+  
     if (windowWidth.value <= 480) {
-        // Assuming state is defined in the component's setup
-        authStore.setIsMobile(true);
+      authStore.setIsMobile(true);
     } else {
-         // Assuming state is defined in the component's setup
-         authStore.setIsMobile(false);
-        mobileNav.value = false;
+      authStore.setIsMobile(false);
+      mobileNav.value = false;
     }
-}
-
-function useWindowResize() {
-
-
-
-// Call onMounted hook
-onMounted(() => {
-    // Attach event listener
-    window.addEventListener('resize', handler);
-});
-
-onUnmounted(() => {
-    window.removeEventListener('resize', handler);
-});
-
-return (windowWidth.value, windowHeight.value)
-}
-
-
-</script>
+  }
+  
+  // Window resize listener setup and cleanup
+  function useWindowResize() {
+    onMounted(() => {
+      window.addEventListener('resize', handler);
+    });
+  
+    onUnmounted(() => {
+      window.removeEventListener('resize', handler);
+    });
+  
+    return windowWidth.value, windowHeight.value;
+  }
+  </script>
+  
+  <style scoped>
+  /* Additional styles if necessary */
+  </style>
+  
