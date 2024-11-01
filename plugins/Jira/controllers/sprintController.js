cad4b9e4e8f7c71ac561f8f32114e95c91430fc0
Steve Browning <foozlereducers@gmail.com>
2024-11-01 10:14:38 -0400

    Added getProjects() and getBoardIds apis, contorllers and added them to the Sprint class. The projects is a reduced JSON object that has the team name, boardIds and other info. The boardIds is an array of just boardIds. I fixed isLoading in the Nav

--- plugins/Jira/controllers/sprintController.js
+++ plugins/Jira/controllers/sprintController.js
@@ -1,9 +1,23 @@
-export const triggerNewSprint = (sprintInstance) => async (req, res, next) => {
+
+
+export const getSprint = (sprintInstance) => async (req, res, next) => {
     try {
         const { boardId } = req.query;
         const sp = sprintInstance;
-        const result = await sp.createSprint(boardId);
-        console.log('in Sprint controller', result)
+        const result = await sp.consolidateSprint(boardId);
+        // Send the result back as JSON
+        res.status(200).json(result);
+    } catch (error) {
+        console.error('Error in triggerNewSprint:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+export const getSprintStoryPoints = (sprintInstance) => async (req, res, next) => {
+    try {
+        const { boardId } = req.query;
+        const sp = sprintInstance;
+        const sprintValues = await sp.consolidateSprint(boardId);
+        
         // Send the result back as JSON
         res.status(200).json(result);
     } catch (error) {
@@ -12,3 +26,14 @@ export const triggerNewSprint = (sprintInstance) => async (req, res, next) => {
     }
 }
 
+export const getProjects = (sprintInstance) => async (req, res, next) => {
+    const sp = sprintInstance;
+    const projs = await sp.getProjects()
+    res.status(200).json(projs)
+}
+
+export const getBoardIds = (sprintInstance) => async (req, res, next) => {
+    const sp = sprintInstance;
+    const boardIds = await sp.getBoardIds()
+    res.status(200).json(boardIds)
+}
