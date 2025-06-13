436a9583422a47f1e8d294f6ed0725b23f66a9f6
Steve Browning <sbrowning@engineeredintel.com>
2025-06-13 16:54:29 -0400

    Removed logos

--- views/frontEnd/src/components/Icon.vue
+++ views/frontEnd/src/components/Icon.vue
@@ -1,3 +1,3 @@
 <template>
-    <img class="acto-icon" src="../assets/images/Acto_logo_teal.svg" alt="Icon of ACTO's logo with a 3 barchart bars">
+   <-- your logo image to go into the header. Preferrably a .svg so you can nicely scale your logo -/>
 </template>
